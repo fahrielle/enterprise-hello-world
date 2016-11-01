58a7a9821665aa65f35aeddc8a81df5ca52ad7ab
damien-dg <damien.doucet-girard@mail.mcgill.ca>
2016-10-31 23:12:02 -0400

    informative comments!

--- src/HelloWorldMainThread.java
+++ src/HelloWorldMainThread.java
@@ -2,9 +2,10 @@ import v102316.HelloWorldMain;
 
 public class HelloWorldMainThread implements Runnable{
     public void run() {
+    	//Call our main helloWorld's main class's main
 		HelloWorldMain.main(null);
     }
-
+    //Start our main helloWorld's main class's main execution thread
     public static void main(String args[]) {
         (new Thread(new HelloWorldMainThread())).start();
     }
